29ff6349d77267ae4b7b44db9b13421bc73bb953
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2020-12-09 16:35:15 +0000

    Bump Clojure version to latest
    That is true that Clojure is stable and old Clojure programs can run
    perfectly well after seven years, but still ^^

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :scm {:name "git" :url "http://github.com/piotr-yuxuan/gzip-util.git"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]])
